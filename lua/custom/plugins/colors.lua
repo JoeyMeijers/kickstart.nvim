984661c31c2cdd80f777d3173a9c5626b26f13d8
Joey <joeymeij@gmail.com>
2025-01-17 20:56:19 +0100

    set color theme in colors.lua

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,10 +1,12 @@
 return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
-  config = function()
-    vim.cmd 'colorscheme rose-pine'
-    require('rose-pine').setup {
-      disable_background = true,
-    }
-  end,
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      vim.cmd 'colorscheme rose-pine'
+      require('rose-pine').setup {
+        disable_background = true,
+      }
+    end,
+  },
 }
